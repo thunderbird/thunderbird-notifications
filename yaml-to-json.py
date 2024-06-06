94f47d80845c5aada5948f4a369ec2aad9f43997
Chris Aquino <chris@thunderbird.net>
2024-06-06 14:05:58 -0400

    Use NotificationSchema to validate incoming yaml and produce json

--- yaml-to-json.py
+++ yaml-to-json.py
@@ -1,7 +1,7 @@
 import yaml
-import json
 import glob
 import os
+from NotificationSchema import NotificationSchema
 
 YAML_DIR = 'yaml_notifications'
 JSON_DIR = 'json_notifications'
@@ -28,32 +28,22 @@ class YAMLtoJSONConverter:
       return None
     return f'{os.path.basename(base_name)}.json'
 
-  def yaml_to_json(self, in_file):
+  def write_schema_as_json(self, schema, out_file):
     try:
-      return yaml.safe_load(in_file)
-    except yaml.YAMLError as e:
-      print(f'Error parsing YAML file: {e}')
-      return None
-
-  def write_json(self, data_from_yaml, out_file):
-    try:
-      json.dump(data_from_yaml, out_file, indent=2)
+      out_file.write(schema.model_dump_json(indent=2))
     except Exception as e:
       print(f'Error writing JSON file: {e}')
 
   def convert(self):
     for in_fname in self.get_yaml_files():
-      with open(in_fname, "r") as in_file:
-        data_from_yaml = self.yaml_to_json(in_file)
-        if not data_from_yaml:
-          continue
+      schema = NotificationSchema.from_yaml(in_fname)
 
-        out_fname = self.generate_json_name(in_fname)
-        if out_fname:
-          out_path = os.path.join(self.json_dir, out_fname)
-          if not os.path.exists(out_path):
-            with open(out_path, "w") as f:
-              self.write_json(data_from_yaml, f)
+      out_fname = self.generate_json_name(in_fname)
+      if out_fname:
+        out_path = os.path.join(self.json_dir, out_fname)
+        if not os.path.exists(out_path):
+          with open(out_path, "w") as f:
+            self.write_schema_as_json(schema, f)
 
 def main():
   converter = YAMLtoJSONConverter(YAML_DIR, JSON_DIR)
