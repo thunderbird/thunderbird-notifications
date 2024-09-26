af8950ac0426db99b3cc6f91866117aaa3cd0861
Chris Aquino <chris@thunderbird.net>
2024-09-26 13:01:03 -0400

    Captures less formal conventions via description kwargs

--- NotificationSchema.py
+++ NotificationSchema.py
@@ -13,6 +13,7 @@ class ChannelEnum(str, Enum):
     beta = "beta"
     daily = "daily"
 
+
 class OperatingSystemEnum(str, Enum):
     win = "win"
     macosx = "macosx"
@@ -24,11 +25,11 @@ class OperatingSystemEnum(str, Enum):
     other = "other"
 
 class Profile(BaseModel):
-    displayed_notifications: list[str] | None = None
-    locales: list[str] | None = None
-    versions: list[str] | None = None
-    channels: list[ChannelEnum] | None = None
-    operating_systems: list[OperatingSystemEnum] | None = None
+    displayed_notifications: Optional[list[str]] = Field(default=None, description="An id of an event previously displayed by the client.")
+    locales: Optional[list[str]] = Field(default=None, description="The selected UI language of the client.")
+    versions: Optional[list[str]] = Field(default=None, description="A client application version string.")
+    channels: Optional[list[ChannelEnum]] = Field(default=None, description="The client release channel name.")
+    operating_systems: Optional[list[OperatingSystemEnum]] = Field(default=None, description="The operating system the client is running under.")
 
 class SeverityEnum(int, Enum):
     one = 1
@@ -46,20 +47,20 @@ class TypeEnum(str, Enum):
 
 class Targeting(BaseModel):
     percent_chance: Optional[float] = Field(None, ge=0, le=100)
-    exclude: list[Profile] | None = None
-    include: list[Profile] | None = None
+    exclude: Optional[list[Profile]] = Field(default=None)
+    include: Optional[list[Profile]] = Field(default=None)
 
 class Notification(BaseModel):
-    id: UUID
-    start_at: datetime
-    end_at: datetime
-    title: str
-    description: str
-    URL: HttpUrl | None = None
-    CTA: str | None = None
-    severity: SeverityEnum
-    type: TypeEnum
-    targeting: Targeting
+    id: UUID = Field(..., description="Unique ID set by the server")
+    start_at: datetime = Field(..., description="UTC Timestamp after which Thunderbird will show the event after startup")
+    end_at: datetime = Field(..., description="UTC Timestamp after which Thunderbird will never show the event, even if it has never been shown to the user.")
+    title: str = Field(..., description="Short sentence describing the event which will be displayed in the Thunderbird UI.")
+    description: str = Field(..., description="A short paragraph that can contain HTML and will be displayed in the Thunderbird UI.")
+    URL: Optional[HttpUrl] = Field(default=None, description="URL to open from the CTA, if any.")
+    CTA: Optional[str] = Field(default=None, description="Link text to show for the URL.")
+    severity: SeverityEnum = Field(..., description="Severity level, where 1 is the most important/urgent and 5 is the least.")
+    type: TypeEnum = Field(..., description="Category of notification.")
+    targeting: Targeting = Field(..., description="Targeting criteria for the notification")
 
     @model_validator(mode="after")
     def cta_requires_url(self):
@@ -67,9 +68,10 @@ class Notification(BaseModel):
             raise ValueError("if 'CTA' is present, 'URL' must be present too")
         return self
 
-class NotificationSchema(RootModel):
+class NotificationModel(RootModel):
   root: list[Notification]
 
+  @staticmethod
   def yaml_to_data(yaml_str):
     """Static method to load YAML from a string and return the corresponding python object"""
     try:
@@ -78,16 +80,17 @@ class NotificationSchema(RootModel):
       print(f'Error parsing YAML file: {e}')
       return None
 
+  @staticmethod
   def from_yaml(file_name):
     """Static method to generate NotificationSchema from a yaml file"""
     with open(file_name, "r") as fh:
       contents = fh.read()
-      data = NotificationSchema.yaml_to_data(contents)
-      return NotificationSchema(data)
+      data = NotificationModel.yaml_to_data(contents)
+      return NotificationModel(data)
 
+  @staticmethod
   def generate_json_schema(schema_file_name):
       """Static method to write a JSON schema file based on pydantic model"""
-      schema = NotificationSchema.model_json_schema()
+      schema = NotificationModel.model_json_schema()
       with open(schema_file_name, "w") as f:
             f.write(json.dumps(schema, indent=2))
-
